8adef82d13f9d1a47a728d0eac498156bdb3d242
jnllyng <jueunyang00@gmail.com>
2026-03-23 02:11:40 -0500

    Add books controller, index and show views

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,19 @@
 class BooksController < ApplicationController
   def index
+    if params[:search].present?
+      @books = Book.joins(:author, :genre)
+                   .where("books.title LIKE ? OR authors.name LIKE ?",
+                          "%#{params[:search]}%",
+                          "%#{params[:search]}%")
+                   .includes(:author, :genre)
+                   .page(params[:page]).per(12)
+    else
+      @books = Book.all.includes(:author, :genre).page(params[:page]).per(12)
+    end
   end
 
   def show
+    @book = Book.find(params[:id])
+    @reviews = @book.reviews
   end
 end
