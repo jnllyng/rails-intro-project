f60401f711a83fa1b0f1c963bfea603443701daf
jnllyng <jueunyang00@gmail.com>
2026-03-23 02:50:19 -0500

    Update books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,18 @@
 class BooksController < ApplicationController
   def index
-    if params[:search].present?
-      @books = Book.joins(:author, :genre)
-                   .where("books.title LIKE ? OR authors.name LIKE ?",
-                          "%#{params[:search]}%",
-                          "%#{params[:search]}%")
-                   .includes(:author, :genre)
-                   .page(params[:page]).per(12)
-    else
-      @books = Book.all.includes(:author, :genre).page(params[:page]).per(12)
-    end
+  @genres = Genre.all
+  if params[:search].present?
+    @books = Book.joins(:author, :genre)
+                 .where("books.title LIKE ? OR authors.name LIKE ?",
+                        "%#{params[:search]}%",
+                        "%#{params[:search]}%")
+                 .includes(:author, :genre)
+    @books = @books.where(genre_id: params[:genre_id]) if params[:genre_id].present?
+    @books = @books.page(params[:page]).per(12)
+  else
+    @books = Book.all.includes(:author, :genre).page(params[:page]).per(12)
   end
+end
 
   def show
     @book = Book.find(params[:id])
