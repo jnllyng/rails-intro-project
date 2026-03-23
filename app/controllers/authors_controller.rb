6ff028a64365b4c441bfca966609f5bfb60e5c81
jnllyng <jueunyang00@gmail.com>
2026-03-23 02:13:48 -0500

    Add authors controller, index and show views

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,7 +1,10 @@
 class AuthorsController < ApplicationController
   def index
+    @authors = Author.all.includes(:books)
   end
 
   def show
+    @author = Author.find(params[:id])
+    @books = @author.books.includes(:genre)
   end
 end
--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,7 +1,10 @@
 class GenresController < ApplicationController
   def index
+    @genres = Genre.all.includes(:books)
   end
 
   def show
+    @genre = Genre.find(params[:id])
+    @books = @genre.books.includes(:author)
   end
 end
